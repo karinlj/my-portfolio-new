f8ca5b8b3e2d8162f62285785db8a9f75aed6ec6
ljunggren <mljunggren@gmail.com>
2024-04-23 17:38:54 -0400

    Added files

--- start.sh
+++ start.sh
@@ -3,8 +3,11 @@
 # Set the port number from the first argument, default to 3000 if not provided
 PORT=6666
 
+# Define the log file
+LOG_FILE="nohup-${PORT}.log"
+
 # Run npm run dev in the background
-nohup npm run dev -- --port=$PORT &> /dev/null &
+nohup npm run dev -- --port=$PORT &> "${LOG_FILE}" &
 
 # Save the PID of the background process to a file
 echo $! > "npm-${PORT}.pid"
